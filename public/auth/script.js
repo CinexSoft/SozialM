f3d36d655a659709bd77cf6764f185d2ade188a8
OogleGlu <avirukbasak@gmail.com>
2021-09-10 18:49:44 +0530

    Updated files

--- public/auth/script.js
+++ public/auth/script.js
@@ -1,6 +1,8 @@
 // root visibility flag
 let rootFlag = "login";
 const toogleRoot = () => {
+    $("#signup_info").style.display = "none";
+    $("#login_info").style.display = "none";
     if (rootFlag == "login") {
         $("#login").style.display = "none";
         $(".swinfo")[0].innerHTML = "Already have an account? ";
@@ -37,46 +39,56 @@ const togglePass = () => {
 }
 // login button clicked
 $("#btn_login").addEventListener("click", (e) => {
-    $("#login_err").style.display = "none";
+    $("#login_info").style.color = "#555";
+    $("#login_info").innerHTML = "Logging you in, please wait...";
+    $("#login_info").style.display = "block";
     email = $("#login_email").value;
     password = $("#login_pass").value;
     auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
         let user = userCredential.user;
         localStorage.setItem("User.isLoggedIn", "true");
-        localStorage.setItem("userdata.uid", user.uid);
+        localStorage.setItem("user.credentials", JSON.stringify(user));
+        location.href = "/";
     })
     .catch((error) => {
-        $("#login_err").innerHTML = ((error.code != "auth/operation-not-allowed" &&
+        $("#login_info").style.color = "red";
+        $("#login_info").innerHTML = ((error.code != "auth/operation-not-allowed" &&
                                       error.code != "auth/invalid-argument" &&
                                       error.code != "auth/internal-error") ?
-                                      error.message : "An error occurred, try again later").slice(0, 64) + "...";
-        $("#login_err").style.display = "block";
-        err("Error: " + error.message + " code: " + error.code);
+                                      error.message : "An error occurred, try again later").slice(0, 65) +
+                                     (error.message.length > 64 ? "..." : "");
+        $("#login_info").style.display = "block";
+        err("Code: " + error.code + " msg: " + error.message);
     });
 });
 // signup button clicked
 $("#btn_signup").addEventListener("click", (e) => {
-    $("#signup_err").style.display = "none";
+    $("#signup_info").style.color = "#555";
+    $("#signup_info").innerHTML = "Signing you up, please wait...";
+    $("#signup_info").style.display = "block";
     email = $("#signup_email").value;
     if ($("#signup_pass").value !=
         $("#signup_pass_c").value) {
-        $("#signup_err").style.display = "block";
-        $("#signup_err").innerHTML = "Password don't match";
+        $("#signup_info").style.display = "block";
+        $("#signup_info").innerHTML = "Password don't match";
         return;
     }
     password = $("#signup_pass").value;
     auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
         let user = userCredential.user;
         localStorage.setItem("User.isLoggedIn", "true");
-        localStorage.setItem("userdata.uid", user.uid);
+        localStorage.setItem("user.credentials", JSON.stringify(user));
+        location.href = "/";
     })
     .catch((error) => {
-        $("#login_err").innerHTML = ((error.code != "auth/operation-not-allowed" &&
+        $("#signup_info").style.color = "red";
+        $("#signup_info").innerHTML = ((error.code != "auth/operation-not-allowed" &&
                                       error.code != "auth/invalid-argument" &&
                                       error.code != "auth/internal-error") ?
-                                      error.message : "An error occurred, try again later").slice(0, 64) + "...";
-        $("#signup_err").style.display = "block";
-        err("Error: " + error.message + " code: " + error.code);
+                                      error.message : "An error occurred, try again later").slice(0, 65) +
+                                     (error.message.length > 64 ? "..." : "");
+        $("#signup_info").style.display = "block";
+        err("Code: " + error.code + " msg: " + error.message);
     });
 });
 log("document and script load complete");
