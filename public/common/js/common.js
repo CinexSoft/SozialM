49bef53686d2c422b0def168eabc83814a1b519a
OogleGlu <avirukbasak@gmail.com>
2021-09-19 20:44:24 +0530

    Overlays are now rendered properly on Firefox

--- public/common/js/common.js
+++ public/common/js/common.js
@@ -35,8 +35,7 @@ document.body.addEventListener('click', (e) => {
     log(`common.js: click: id = ${e.target.id} node = ${e.target.nodeName} class = ${e.target.className}`);
     if (['alertDialog_btn', 'actionDialog_btnClose'].includes(e.target.id) && e.target.innerHTML == 'Close') {
         e.target.id.slice(0, 5) == 'alert' ? dialog.hide('alert') : dialog.hide('action');
-    }
-    else if (['menuRoot', 'actionDialogRoot'].includes(e.target.id)) {
+    } else if (['menuRoot', 'actionDialogRoot'].includes(e.target.id)) {
         e.target.id.slice(0, 4) == 'menu' ? menu.hide() : dialog.hide('action');
     }
 });
