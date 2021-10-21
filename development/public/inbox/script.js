3afbfd28a985f10f8e321ce9fbad85ccf57700fb
OogleGlu <avirukbasak@gmail.com>
2021-10-22 01:49:16 +0530

    inbox/script.js: removed unecessary code

--- development/public/inbox/script.js
+++ development/public/inbox/script.js
@@ -1,14 +1,15 @@
-import { Auth, Database, DB_ROOT } from '/common/js/firebaseinit.js';
-import * as FirebaseDatabase from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js';
-import { USER_ID } from '/common/js/variables.js';
+import { Auth, } from '/common/js/firebaseinit.js';
+import { USER_ID, } from '/common/js/variables.js';
 import { log, err, } from '/common/js/logging.js';
 import { checkForApkUpdates, getURLQueryFieldValue, } from '/common/js/generalfunc.js';
 import { $, } from '/common/js/domfunc.js';
 import { Dialog, } from '/common/js/overlays.js';
 
 const storeChatRoomId = (chat_room_id) => {
+    
     chat_room_id = (chat_room_id || 'ejs993ejiei3').replace(/[^A-Za-z0-9]/g, '');
     log(`Inbox: chat room id: ${chat_room_id}`);
+
     // stores the chat room id into localStorage to be used by '/chat'.
     localStorage.setItem('Chat.roomid', chat_room_id);
 }
@@ -36,32 +37,22 @@ const getChatHTML = ({ chat_room_id, dp_src, sender, date_time, message }) => {
 }
 
 const main = () => {
+
     let chat_room_id;
     let chatrooms = [];
-    let all_chat_data = {};
+
     // If chatroom id exists as a URL query field, store it
     if ((chat_room_id = getURLQueryFieldValue('id'))
     && !Array.isArray(chat_room_id)) storeChatRoomId(chat_room_id);
-    // checking if user is logged in
-    if (!localStorage.getItem('Auth.user')) {
-        console.log('Log: not signed in, redirect to /auth');
-        location.href = '/auth';
-        return;
-    }
+
     // If chatroom id already exists in localStorage, go to /chat
     if (chat_room_id = localStorage.getItem('Chat.roomid')) {
         location.href = `/chat?id=${chat_room_id}`;
         return;
     }
-    FirebaseDatabase.get(FirebaseDatabase.ref(`${USER_ROOT}/${USER_ID}/chatrooms`)).then((snapshot) => {
-        for ({ key } of snapshot) {
-            chatrooms.push(key);
-        }
-    }).catch((error) => {
-        displayErrorDialog(error, 'Inbox');
-    });
-    // code to load chats belonging to the user
-    /* If chatroom id doesn't exist, user will be prompted for it.
+
+    /* code to load chats belonging to the user
+     * If chatroom id doesn't exist, user will be prompted for it.
      * this prompt is a temporary code while the inbox is being built
      */
     if (true) Dialog.display('alert', 'Text with',
