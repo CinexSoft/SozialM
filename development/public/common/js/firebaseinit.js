b1f39bbac9236940a5603768b1546e70d2869a85
OogleGlu <avirukbasak@gmail.com>
2021-10-30 14:35:11 +0530

    renamed sozialnmedien to sozialm

--- development/public/common/js/firebaseinit.js
+++ development/public/common/js/firebaseinit.js
@@ -7,17 +7,17 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const FirebaseConfig = {
     apiKey: 'AIzaSyBeOE11fAnnEQTimqK8VClhlWMzyOu3ob8',
-    authDomain: 'sozialnmedien.firebaseapp.com',
-    databaseURL: 'https://sozialnmedien-default-rtdb.firebaseio.com',
-    projectId: 'sozialnmedien',
-    storageBucket: 'sozialnmedien.appspot.com',
+    authDomain: 'sozialm.firebaseapp.com',
+    databaseURL: 'https://sozialm-default-rtdb.firebaseio.com',
+    projectId: 'sozialm',
+    storageBucket: 'sozialm.appspot.com',
     messagingSenderId: '584583202268',
     appId: '1:584583202268:web:60e4997e7a59138bdfbb19',
     measurementId: 'G-MFG92Y4C4F',
 };
 
 // If hosted on localhost, use database at localhost
-if (/localhost|127\.0.\.0\.1/i.test(location.href)) FirebaseConfig.databaseURL = 'http://localhost:9000/?ns=sozialnmedien';
+if (/localhost|127\.0.\.0\.1/i.test(location.href)) FirebaseConfig.databaseURL = 'http://localhost:9000/?ns=sozialm';
 
 // Initialize Firebase
 export const App = initializeApp(FirebaseConfig);
@@ -27,9 +27,9 @@ export const Auth = getAuth(App);
 /* Seperates roots for preview and production databases.
  * This code checks if the URL is the production URL and accordingly sets the
  * database root.
- * Production URLs are sozialnmedien.web.app and sozialnmedien.firebaseapp.com
+ * Production URLs are sozialm.web.app and sozialm.firebaseapp.com
  */
-const ROOT = (!/sozialnmedien\.web\.app|sozialnmedien\.firebaseapp\.com/i.test(location.href) ? '/preview' : '/production');
+const ROOT = (!/sozialm\.web\.app|sozialm\.firebaseapp\.com/i.test(location.href) ? '/preview' : '/production');
 
 export const RTDB_USERS_ROOT = ROOT + '/aa14fdd9-users';
 export const RTDB_SLOGS_ROOT = ROOT + '/b6d6cc89-slogs';
